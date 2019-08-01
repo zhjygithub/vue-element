c5a7451c3a99c0647b87dee5dacb241272ba8a0a
zhjygithub <zhjy_my520@foxmail.com>
2019-08-01 09:55:21 +0800

    add baseUrl

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const urlMap ={
   development:'/',
-  production:'http://www.jiguangcloud.com:8900/'
+  production:'www.jiguangcloud.com:8900/'
 }
 const baseUrl = urlMap[process.env.NODE_ENV]
 const ERR_OK = 0
